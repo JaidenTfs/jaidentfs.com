664928d2c73899bd41473e25dee577a3072bc43a
JaidenTfs <78136587+JaidenTfs@users.noreply.github.com>
2024-07-14 16:55:20 -0600

    Added music player elements
    Specifically for portfolio useage

---
+++ html-projects/albums.js
@@ -0,0 +1,180 @@
+function albumRoar() {
+    
+track_list = [
+  {
+    name: "I Can't Handle Change",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/i-cant-handle-change.mp3"
+  },
+  {
+    name: "Duck Or Ape",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/duck-or-ape.mp3"
+  },
+  {
+    name: "Heart For Brains",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/heart-for-brains.mp3"
+  },
+  {
+    name: "Baby Bride Rag",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/baby-bride-rag.mp3"
+  },
+  {
+    name: "Christmas Kids",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/christmas-kids.mp3"
+  },
+  {
+    name: "Just A Fan",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/just-a-fan.mp3"
+  },
+];
+}
+
+function albumMiraclemusical() {
+    
+track_list = [
+  {
+    name: "Introduction to the Snow",
+    artist: "Roar",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/introduction-to-the-snow.mp3"
+  },
+  {
+    name: "Isle Unto Thyself",
+    artist: "Roar",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/isle-unto-thyself.mp3"
+  },
+  {
+    name: "The Mind Electric",
+    artist: "Roar",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/the-mind-electric.mp3"
+  },
+];
+  let bgColor = "rgb(" + 255 + "," + 0 + "," + 0 + ")";
+}
+
+function albumRick() {
+    
+track_list = [
+  {
+    name: "One Way Mirror",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/one-way-mirror.mp3"
+  },
+  {
+    name: "Boy Toy",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/boy-toy.mp3"
+  },
+  {
+    name: "Truth or Dare",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/truth-or-dare.mp3"
+  },
+  {
+    name: "I'm Just Joking in This Interlude
+  },
+  {
+    name: "In Your Pocket",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/in-your-pocket.mp3"
+  },
+  {
+    name: "Don't Say That",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/dont-say-that.jpg",
+    path: "songs/ricky-montgomery/rick/dont-say-that.mp3"
+  },
+  {
+    name: "Eraser",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/eraser.jpg",
+    path: "songs/ricky-montgomery/rick/eraser.mp3"
+  },
+  {
+    name: "We Got Married Twice (interlude)",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/we-got-married-twice.mp3"
+  },
+  {
+    name: "Type A",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/type-a.mp3"
+  },
+  {
+    name: "Paper Towel",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/paper-towel.mp3"
+  },
+  {
+    name: "Sometimes I Need to Be Alone",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/sometimes-i-need-to-be-alone.mp3"
+  },
+  {
+    name: "Ethan's Song",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/ethans-song.mp3"
+  },
+  {
+    name: "Black Fins",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/black-fins.jpg",
+    path: "songs/ricky-montgomery/rick/black-fins.mp3"
+  },
+  {
+    name: "Ribbons (outro)",
+    artist: "Roar",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/ricky-montgomery/rick/ribbons.mp3"
+  },
+
+];
+}
+
+function albumMiraclemusical() {
+    
+track_list = [
+  {
+    name: "Introduction to the Snow",
+    artist: "Roar",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/introduction-to-the-snow.mp3"
+  },
+];
+  let bgColor = "rgb(" + 255 + "," + 0 + "," + 0 + ")";
+}
+
+function albumMakeroom() {
+    
+track_list = [
+  {
+    name: "Introduction to the Snow",
+    artist: "Roar",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/introduction-to-the-snow.mp3"
+  },
+];
+  let bgColor = "rgb(" + 255 + "," + 0 + "," + 0 + ")";
+}
---
+++ html-projects/albums2.js
@@ -0,0 +1,96 @@
+function albumRoar() {
+    
+track_list = [
+  {
+    name: "I Can't Handle Change",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/i-cant-handle-change.mp3"
+  },
+  {
+    name: "Duck Or Ape",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/duck-or-ape.mp3"
+  },
+  {
+    name: "Heart For Brains",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/heart-for-brains.mp3"
+  },
+  {
+    name: "Baby Bride Rag",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/baby-bride-rag.mp3"
+  },
+  {
+    name: "Christmas Kids",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/christmas-kids.mp3"
+  },
+  {
+    name: "Just A Fan",
+    artist: "Roar",
+    image: "covers/roar/i-cant-handle-change.jpg",
+    path: "songs/roar/just-a-fan.mp3"
+  },
+];
+    document.querySelector("body").style.backgroundImage = "url('covers/roar/i-cant-handle-change-background.jpg')";
+    document.querySelector("ul").style.backgroundImage = "url('covers/roar/i-cant-handle-change-banner.jpg')";
+    document.querySelector(".seek_slider").style.background = "#white";
+    document.querySelector(".volume_slider").style.background = "#white";
+    document.querySelector("body").style.color = "white";
+    document.querySelector("body").style.textShadow = "1px 1px 2px black, 0 0 25px white, 0 0 5px black";
+}
+
+function albumMiraclemusical() {
+    
+track_list = [
+  {
+    name: "Introduction to the Snow",
+    artist: "Miracle Musical",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/introduction-to-the-snow.mp3"
+  },
+  {
+    name: "Isle Unto Thyself",
+    artist: "Miracle Musical",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/isle-unto-thyself.mp3"
+  },
+  {
+    name: "The Mind Electric",
+    artist: "Miracle Musical",
+    image: "covers/miracle-musical/hawaii-part-ii.jpg",
+    path: "songs/miracle-musical/the-mind-electric.mp3"
+   },
+];
+    document.body.style.backgroundImage = "linear-gradient(90deg, #9e4248 20%, #9c6944 30.3%, #9B9C45 41.7%, #7FA14A 50%, #549556 58.3%, #3F806A 66.7%, #3E5F8F 75%, #4350A9 83.3%, #5348A9 91.7%, #6D48A9 100%)";
+
+    document.querySelector("ul").style.backgroundImage = "url('covers/miracle-musical/hawaii-part-ii-banner.jpg')";
+    document.querySelector(".seek_slider").style.background = "#6D48A9";
+    document.querySelector(".volume_slider").style.background = "#6D48A9";
+    document.querySelector("body").style.color = "white";
+    document.querySelector("body").style.textShadow = "1px 1px 2px black, 0 0 25px #6D48A9, 0 0 5px black";
+}
+
+function albumRick() {
+    
+track_list = [
+  {
+    name: "One-Way Mirror",
+    artist: "Ricky Montgomery",
+    image: "covers/ricky-montgomery/rick.jpg",
+    path: "songs/miracle-musical/introduction-to-the-snow.mp3"
+  },
+];
+    document.querySelector("body").style.backgroundImage = "url('covers/ricky-montgomery/rick-background.jpg')";
+    document.querySelector("ul").style.backgroundImage = "url('covers/ricky-montgomery/rick-banner.jpg')";
+    document.querySelector(".seek_slider").style.background = "#B52616";
+    document.querySelector(".volume_slider").style.background = "#B52616";
+    document.querySelector("body").style.color = "#B52616";
+    document.querySelector("body").style.textShadow = "1px 1px 2px white, 0 0 15px black, 0 0 5px black";
+}
