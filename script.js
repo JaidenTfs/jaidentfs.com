4adfdacf7929a4d632e23726226387994b6c4200
JaidenTfs <78136587+JaidenTfs@users.noreply.github.com>
2024-10-02 12:30:16 -0600

    Added image like in the carrd

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 //Javascript!
-use App\Livewire\Dashboard;
-use App\Livewire\ShowPosts;
-use App\Livewire\ShowUsers;
- 
-Route::get('index.html', Dashboard::class);
- 
-Route::get('portfolio.html', ShowPosts::class);
- 
-Route::get('contact.html', ShowUsers::class);
+console.log ("JaidenTfs")
