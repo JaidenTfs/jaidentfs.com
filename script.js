726c1b119179e48cd579a6c0cfcaae88aa437c31
JaidenTfs <78136587+JaidenTfs@users.noreply.github.com>
2024-10-23 10:14:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //Javascript!
-console.log ("JaidenTfs")
+console.log ("JaidenTfs");
+ window.onload = function() 
+  { document.getElementById("hideAll").style.display = "none"; 
+    console.log (loaded HTML);}
