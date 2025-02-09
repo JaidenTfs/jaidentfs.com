a4442d2ec8d2d5832e613f9f6d971c77b6f36081
SickkMade <66890134+SickkMade@users.noreply.github.com>
2025-02-09 12:47:33 -0800

    Slight UI Changes (#1)
    * Save my changes
    
    * changed some slight ui

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Javascript!
-console.log ("JaidenTfs");
- window.onload = function() 
-  { document.getElementById("hideAll").style.display = "none"; 
-    console.log (Loading Main HTML);}
+// //Javascript!
+// console.log ("JaidenTfs");
+//  window.onload = function() 
+//   { document.getElementById("hideAll").style.display = "none"; 
+//     console.log (Loading Main HTML);}
