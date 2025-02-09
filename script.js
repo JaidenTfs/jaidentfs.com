714823d9c3b888d6f0434009ffa2304b9e4d1113
Jaiden Everett <78136587+JaidenTfs@users.noreply.github.com>
2025-02-09 15:22:03 -0700

    Test at only letting one audio play at a time

--- script.js
+++ script.js
@@ -3,3 +3,21 @@
 //  window.onload = function() 
 //   { document.getElementById("hideAll").style.display = "none"; 
 //     console.log (Loading Main HTML);}
+
+// Test at only letting one audio play at a time
+function onlyPlayOneIn(container) {
+  container.addEventListener("play", function(event) {
+  audio_elements = container.getElementsByTagName("audio")
+    for(i=0; i < audio_elements.length; i++) {
+      audio_element = audio_elements[i];
+      if (audio_element !== event.target) {
+        audio_element.pause();
+      }
+    }
+  }, true);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  onlyPlayOneIn(document.body);
+});
+// End of the test
