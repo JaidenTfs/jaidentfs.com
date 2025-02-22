9e24185c83971884ef28eb441eae4d255b67d306
Jaiden Everett <78136587+JaidenTfs@users.noreply.github.com>
2025-02-21 22:55:29 -0700

    Test

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let audioIndex = localStorage.getItem("audioIndex") !== null ? parseInt(localSto
 let isPlaying = localStorage.getItem("isPlaying") === "true";
 let updateTimer;
 
-if (localStorage.getItem("currentMinutes") !== null) {
-  getTimestamps();
-}
+getTimestamps();
 
 let project_image = document.querySelector(".project-image");
 let audio_title = document.querySelector(".audio-title");
